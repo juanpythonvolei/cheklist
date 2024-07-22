61cece36724578290b7c4e090b058c0ea0169397
juanpythonvolei <juanzsalca@outlook.com>
2024-07-21 21:03:56 -0300

    Add files via upload

---
+++ cheklist-main/Checklist/pages/Endereço.py
@@ -0,0 +1,23 @@
+import qrcode
+import streamlit as st
+# URL que você deseja codificar no QR code
+
+image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
+
+url = "https://cheklist-5zyysp6sntm2pdtcfnssg3.streamlit.app/?embed_options=dark_theme"
+
+# Cria o QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(url)
+qr.make(fit=True)
+
+# Cria uma imagem do QR code
+img = qr.make_image(fill_color="black", back_color="white")
+
+
+Codigo = st.image(img)
