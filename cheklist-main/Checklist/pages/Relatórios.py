bbbb64b27a0a8b180b09affbd2e3f0a06c3a24f8
juanpythonvolei <juanzsalca@outlook.com>
2024-07-23 21:02:49 -0300

    Add files via upload

---
+++ cheklist-main/Checklist/pages/Relatórios.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import pandas as pd
+from teste import enviar_emaail,enviar_emaail2
+from fpdf import FPDF
+from io import BytesIO
+from datetime import datetime
+import pytz
+from Estatísticas import estatistica
+import requests
+
+
+
+
+image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
+lista_nomes = []
+requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+roteiro = requiscao.json()
+dados = roteiro['Checklists']
+for item in dados:
+                            Checklist = dados[f'{item}']
+                            for elemento in Checklist:
+                                   espec = Checklist[f'{elemento}']
+                                   itens_ok = espec['Items ok']
+                                   itens_anormais = espec['Items Anormais']
+st.write(dados)
