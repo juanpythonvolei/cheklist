20b5d51723a1467ca73388d73a785c6e6d5a2244
juanpythonvolei <juanzsalca@outlook.com>
2024-07-23 21:14:27 -0300

    Update Estatísticas.py

--- cheklist-main/Checklist/Estatísticas.py
+++ cheklist-main/Checklist/Estatísticas.py
@@ -40,5 +40,5 @@ def estatistica(sim,nao,usuario,data):
     else:
         pass
     ref = db.reference('Checklists')
-    dict_dados = {'Usuário':usuario,'Items ok':sim,'Items Anormais':nao,'Data':data}
+    dict_dados = {'Usuário':usuario,'ok':sim,'Anormais':nao,'Data':data}
     ref.child(data).push().set(dict_dados)
