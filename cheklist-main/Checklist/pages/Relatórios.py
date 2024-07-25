1560fe20b1f90ebb057da5ca4ad65ff44c43f595
juanpythonvolei <juanzsalca@outlook.com>
2024-07-24 22:03:57 -0300

    Update Relatórios.py

--- cheklist-main/Checklist/pages/Relatórios.py
+++ cheklist-main/Checklist/pages/Relatórios.py
@@ -8,13 +8,14 @@ import pytz
 from Estatísticas import estatistica
 import requests
 import google.generativeai as genai
+from streamlit_option_menu import option_menu
 
 lista_item_repetido =[]
 lista_normais = []
 lista = []
 lista_problema = []
 image = st.image('https://www.logolynx.com/images/logolynx/fe/fe346f78d111e1d702b44186af59b568.jpeg')
-opcao_selecionada = st.selectbox("Selecione uma data",['Dados Gerais','Item com mais ocorrências'])
+opcao_selecionada = st.selectbox("Selecione uma data",['Dados Gerais','Item com mais ocorrências','Ver Checklists'])
 lista_nomes = []
 requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
 roteiro = requiscao.json()
@@ -78,3 +79,27 @@ elif opcao_selecionada == 'Item com mais ocorrências':
   response = chat.send_message(f'Analisando os problemas relatados a seguir, me diga qual o problema, como óleo, rodas etc,que mais se repete dentre as queixas:\n\n{texto_problemas}\n')
   resposta = response.text
   st.write(f'{resposta}')
+elif opcao_selecionada == 'Ver Checklists':
+  seletor  = option_menu("Menu Principal", ["Juan Zonho", "Jonatan Lima","Cesar Fusel","Luiz Felipe"], icons=["truck", "map",'robot'], default_index=1)
+  lista_item_repetido =[]
+  lista_normais = []
+  lista = []
+  lista_problema = []
+  texto_problemas = ''
+  requiscao = requests.get('https://bancodedadosroteirooficial-default-rtdb.firebaseio.com/.json')
+  roteiro = requiscao.json()
+  dados = roteiro['Checklists']
+  for item in dados:
+                                lista.append(item)          
+                                Checklist = dados[f'{item}']
+                                for elemento in Checklist:
+                                         espec = Checklist[f'{elemento}']
+                                         Data = espec['Data']
+                                         lista_ok  = espec['ok']
+                                         for item in lista_ok:
+                                              if item  != '...':
+                                                lista_normais.append(item)
+                                         lista_anormal = espec['Anormais'] 
+                                         for item in lista_anormal:
+                                              if item  != '...':
+                                                lista_problema.append(item) 
