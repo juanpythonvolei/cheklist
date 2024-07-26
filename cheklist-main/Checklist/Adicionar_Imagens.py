fd267ec5e596229bb81edd65131c2c295d9afe02
juanpythonvolei <juanzsalca@outlook.com>
2024-07-26 20:29:37 -0300

    Update Adicionar_Imagens.py

--- cheklist-main/Checklist/Adicionar_Imagens.py
+++ cheklist-main/Checklist/Adicionar_Imagens.py
@@ -1,5 +1,7 @@
 import streamlit as st
-image = st.camera_input()
-with open('captured_image.jpg', 'wb') as f:
-    f.write(image.getvalue())
-link = f"./captured_image.jpg"
+
+def adicionar_imagem(nome_texto,nome_link):
+    image = st.camera_input(key=f'{nome_texto}')
+    with open(f'captured_image_{nome_link}.jpg', 'wb') as f:
+        f.write(image.getvalue())
+    link = f"./captured_image_{nome_link}.jpg"
