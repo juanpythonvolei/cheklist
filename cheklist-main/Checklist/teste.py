c71f3820b684706c6dcc8a4f3fbbd9ea81de3dcf
juanpythonvolei <juanzsalca@outlook.com>
2024-07-26 23:32:51 -0300

    Update teste.py

--- cheklist-main/Checklist/teste.py
+++ cheklist-main/Checklist/teste.py
@@ -82,8 +82,8 @@ def enviar_emaail2(dados,usuario,pdf_buffer):
     """
     for item in lista:
         caminho_imagem = item
-        imagem_html = f'<img src="{caminho_imagem}" alt="Imagem {index}">'
-        tabela_html = tabela_html.replace(f"<td>{index}</td>", f"<td>{imagem_html}</td>")
+        imagem_html = f'<img src="{caminho_imagem}" alt="Imagem {item}">'
+        tabela_html = tabela_html.replace(f"<td>{item}</td>", f"<td>{imagem_html}</td>")
     # Anexa a parte HTML ao email
     part = MIMEText(html, "html")
     msg.attach(part)
