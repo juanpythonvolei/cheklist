64e181e7a40f75c585a63b52ae1d101935d2defd
juanpythonvolei <juanzsalca@outlook.com>
2024-07-27 20:18:43 -0300

    Update Checklist.py

--- cheklist-main/Checklist/Checklist.py
+++ cheklist-main/Checklist/Checklist.py
@@ -80,7 +80,7 @@ def criar_pdf_em_memoria(dados):
     pdf.add_table(df)
     pdf.add_page()  # Isso cria uma nova página        
     pdf.add_text('Imagens das observações abaixo')        
-    pdf.add_images_in_rows(st.session_state.lista_imagens, num_images_per_row=3, x_start=10, y_start=90, width=40, height=40, spacing=5)
+    pdf.add_images_in_rows(st.session_state.lista_imagens, num_images_per_row=3, x_start=10, y_start=40, width=30, height=30, spacing=10)
     pdf_buffer = BytesIO()
     pdf_buffer.write(pdf.output(dest='S').encode('latin1'))
     pdf_buffer.seek(0)
