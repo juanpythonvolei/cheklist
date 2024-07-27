49ea88e38f742c73a1023751c1fb483a82e8bed9
juanpythonvolei <juanzsalca@outlook.com>
2024-07-27 13:11:10 -0300

    Update Checklist.py

--- cheklist-main/Checklist/Checklist.py
+++ cheklist-main/Checklist/Checklist.py
@@ -68,17 +68,17 @@ def criar_pdf_em_memoria(dados):
                                         self.image(imagens_validas[i], x=self.w - col_width, y=self.y, w=60, h=40)
                                     self.ln()
 
-            lista_de_imagens = st.session_state.lista_imagens
-            pdf = PDF()
-            pdf.add_table(df, lista_de_imagens)
-            
-            pdf_buffer = BytesIO()
-            pdf.output(pdf_buffer)
-            pdf_buffer.seek(0)
-            
-            return pdf_buffer
+                            lista_de_imagens = st.session_state.lista_imagens
+                            pdf = PDF()
+                            pdf.add_table(df, lista_de_imagens)
+                            pdf_buffer = BytesIO()
+                            pdf.output(pdf_buffer)
+                            pdf_buffer.seek(0)
+                            return pdf_buffer          
+                                                                                         
+                     
 
-
+        
 # Inicializa as listas no estado da sessão
 if 'lista_qtd' not in st.session_state:
     st.session_state.lista_qtd = []
