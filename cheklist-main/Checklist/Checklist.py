c05426d3df108859f6a60ec444ec625f77216f36
juanpythonvolei <juanzsalca@outlook.com>
2024-07-27 12:39:25 -0300

    Update Checklist.py

--- cheklist-main/Checklist/Checklist.py
+++ cheklist-main/Checklist/Checklist.py
@@ -64,7 +64,7 @@ def criar_pdf_em_memoria(dados):
                     if i < len(imagens_validas):
                         self.image(imagens_validas[i], x=self.w - col_width, y=self.y, w=60, h=40)
                     self.ln()
-       df.rename(columns={2: 'Imagens das observações'}, inplace=True)                     
+               df.rename(columns={2: 'Imagens das observações'}, inplace=True)             
     lista_de_imagens = st.session_state.lista_imagens        
     pdf = PDF()
     pdf.add_page()
