2cfa3d08fdf73351244d53f438f9708c981fd7f2
juanpythonvolei <juanzsalca@outlook.com>
2024-07-27 12:02:30 -0300

    Update Checklist.py

--- cheklist-main/Checklist/Checklist.py
+++ cheklist-main/Checklist/Checklist.py
@@ -41,25 +41,27 @@ def criar_pdf_em_memoria(dados):
     df = pd.DataFrame(dados)
 
     class PDF(FPDF):
-                    def header(self):
-                        self.set_font('Arial', 'B', 12)
-                        self.cell(0, 10, f'Relatório de Checklist. Usuário: {usuario}. {data_hora_formatada}', 0, 1, 'C')
-                    def add_table(self, df, lista_de_imagens):
-                            self.set_font('Arial', '', 12)
-                            col_width = 80  # Defina a largura desejada para as células
-                        
-                            for i in range(len(df)):
-                                for j in range(len(df.columns)):
-                                    if j == 2:  # Terceira coluna (índice 2) para as imagens
-                                        # Adicione a imagem à célula
-                                        self.cell(col_width, 10, "", 1)  # Deixe a célula vazia para as imagens
-                                    else:
-                                        self.cell(col_width, 10, str(df.iloc[i, j]), 1)
-                        
-                                # Adicione a imagem à terceira coluna (índice 2)
-                                if i < len(lista_de_imagens):
-                                    self.image(lista_de_imagens[i], x=self.w - col_width, y=self.y, w=col_width, h=10)
-                                self.ln()    
+        def header(self):
+            self.set_font('Arial', 'B', 12)
+            self.cell(0, 10, f'Relatório de Checklist. Usuário: {usuario}. {data_hora_formatada}', 0, 1, 'C')
+
+        def add_table(self, df):
+            self.set_font('Arial', '', 12)
+            col_width = 80  # Defina a largura desejada para as células
+            for i in range(len(df)):
+                for j in range(len(df.columns)):
+                    self.cell(col_width, 10, str(df.iloc[i, j]), 1)
+                self.ln()
+
+    pdf = PDF()
+    pdf.add_page()
+    pdf.add_table(df)
+    
+    pdf_buffer = BytesIO()
+    pdf_buffer.write(pdf.output(dest='S').encode('latin1'))
+    pdf_buffer.seek(0)
+    
+    return pdf_buffer    
             
 
            
