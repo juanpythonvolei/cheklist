f2f08b80568ba3ab7f7eb94de5baf3468fdc64ea
juanpythonvolei <juanzsalca@outlook.com>
2024-07-27 12:06:56 -0300

    Update Checklist.py

--- cheklist-main/Checklist/Checklist.py
+++ cheklist-main/Checklist/Checklist.py
@@ -37,33 +37,41 @@ if usuario:
             st.warning('Novo Checklist Iniciado ✔️')
             st.markdown(f'<div class="my-square">Seja Bem-vindo, {usuario}</div>', unsafe_allow_html=True)
 
-def add_table(self, df, lista_de_imagens):
-    self.set_font('Arial', '', 12)
-    col_width = 80  # Defina a largura desejada para as células
+def criar_pdf_em_memoria(dados):
+    df = pd.DataFrame(dados)
 
-    for i in range(len(df)):
-        for j in range(len(df.columns)):
-            if j == 2:  # Terceira coluna (índice 2) para as imagens
-                # Adicione a imagem à célula
-                self.cell(col_width, 10, "", 1)  # Deixe a célula vazia para as imagens
-            else:
-                self.cell(col_width, 10, str(df.iloc[i, j]), 1)
+    class PDF(FPDF):
+        def header(self):
+            self.set_font('Arial', 'B', 12)
+            self.cell(0, 10, f'Relatório de Checklist. Usuário: {usuario}. {data_hora_formatada}', 0, 1, 'C')
 
-        # Adicione a imagem à terceira coluna (índice 2)
-        if i < len(lista_de_imagens):
-            self.image(lista_de_imagens[i], x=self.w - col_width, y=self.y, w=col_width, h=10)
-        self.ln()
-
-    lista_de_imagens =  st.session_state.lista_imagens      
+        def add_table(self, df, lista_de_imagens):
+                self.set_font('Arial', '', 12)
+                col_width = 80  # Defina a largura desejada para as células
+            
+                for i in range(len(df)):
+                    for j in range(len(df.columns)):
+                        if j == 2:  # Terceira coluna (índice 2) para as imagens
+                            # Adicione a imagem à célula
+                            self.cell(col_width, 10, "", 1)  # Deixe a célula vazia para as imagens
+                        else:
+                            self.cell(col_width, 10, str(df.iloc[i, j]), 1)
+            
+                    # Adicione a imagem à terceira coluna (índice 2)
+                    if i < len(lista_de_imagens):
+                        self.image(lista_de_imagens[i], x=self.w - col_width, y=self.y, w=col_width, h=10)
+                    self.ln()
+    lista_de_imagens = st.session_state.lista_imagens        
     pdf = PDF()
     pdf.add_page()
-    pdf.add_table(df, lista_de_imagens)
+    pdf.add_table(df,lista_de_imagens)
     
     pdf_buffer = BytesIO()
     pdf_buffer.write(pdf.output(dest='S').encode('latin1'))
     pdf_buffer.seek(0)
     
     return pdf_buffer
+
 
 # Inicializa as listas no estado da sessão
 if 'lista_qtd' not in st.session_state:
