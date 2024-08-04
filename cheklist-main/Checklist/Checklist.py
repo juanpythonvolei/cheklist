155e28cc1ad4218a9b55f09c3fdefb2a7af89eff
juanpythonvolei <juanzsalca@outlook.com>
2024-08-04 00:36:58 -0300

    Update Checklist.py

--- cheklist-main/Checklist/Checklist.py
+++ cheklist-main/Checklist/Checklist.py
@@ -38,11 +38,12 @@ if seletor == 'Novo Checklist':
             
             if usuario:
                         senha = st.text_input(label='Insira sua senha')
-                        st.divider() 
+                         
                         for i in lista_usuarios:
                                     login_2,senha_2 = i
                                     if usuario == login_2:
                                                 if senha == senha_2:
+                                                            st.divider()
                                                             st.warning('Acesso autorizado')
                                                             st.session_state.lista_qtd = []
                                                             st.session_state.lista_problemas = []
